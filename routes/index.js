9cb2425a6f0e150c578988bcf2f1d3dee992f7fa
lovejotsingh122 <lovejotsangha122@gmail.com>
2019-02-11 15:12:36 -0500

    added express js support

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+/* GET About Page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
