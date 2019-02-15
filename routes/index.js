34b047fc868797614a43de3f0bc4da527a6bcf78
lovejotsingh122 <lovejotsangha122@gmail.com>
2019-02-15 15:31:51 -0500

    added logo, partial

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,19 @@ router.get('/', function(req, res, next) {
 });
 /* GET About Page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('about', { title: 'About' });
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
