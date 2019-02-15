a85e98f5178635ef816e5ed3af887c5d6ad5834c
lovejotsingh122 <lovejotsangha122@gmail.com>
2019-02-15 14:40:31 -0500

    added partial and some bootstrap

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+//IIFE 
+(function(){
+
+    function Start(){
+console.log('%c App Started...', "font-size=20 px; color:blue; font-weight:bold");
+    }
+    window/addEventListener("load",Start);
+
+})();
